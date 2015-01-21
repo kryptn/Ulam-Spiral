426428bbc2656202d711aa2642ffc3aee082a8ce
kryptn <kryptn@gmail.com>
2015-01-20 21:02:48 -0500

    Combined top and bottom quadrant logic

--- spiral.py
+++ spiral.py
@@ -22,7 +22,7 @@ class Spiral(object):
         if y < 0:
             ry -= 1
         
-        return abs(rx),abs(ry)
+        return rx,ry
         
         
     def position(self, x, y):
@@ -32,14 +32,15 @@ class Spiral(object):
             r = bases[0]**2
         elif x*-1 == y+1 and x >= 0: # odd squares
             r = bases[0]**2
-        elif x*-1 > abs(y): # left quadrant
-            r = (bases[0]-2)**2+1 + bases[0]-y + x-1
-        elif x >= abs(y): # right quadrant
-            r = (bases[0]-2)**2+1 + bases[0]+y - x-3
-        elif y > abs(x): # top quadrant
-            r = bases[1]**2 + bases[1]-x -y*3
-        elif y*-1 >= abs(x): # bottom quadrant
-            r = bases[1]**2 + bases[1]+x +y*3 
+        elif x*-1 > abs(y) or x >= abs(y):
+            r = (abs(bases[0])-2)**2+1 + abs(bases[0])
+            if x < 0: # left quadrant
+                r = r - y + x - 1
+            else: # right quadrant
+                r = r + y - x - 3
+        elif y*-1 >= abs(x) or y > abs(x): # top and bottom
+            lean = bases[1]/abs(bases[1])
+            r = bases[1]**2 + abs(bases[1]) - x*lean - y*lean * 3
         else:
             r = -1
             print >> sys.stderr, "Totally shouldn't happen %d,%d" % (x, y)
