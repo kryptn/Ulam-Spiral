3aeb93268fbd2c9fd887fa2fd770e8696f08bdfb
kryptn <kryptn@gmail.com>
2015-01-23 08:14:03 -0500

    Combined left and right quadrant logic

--- spiral.py
+++ spiral.py
@@ -30,15 +30,12 @@ class Spiral(object):
             r = bases[0]**2
         elif x*-1 == y+1 and x >= 0: # odd squares
             r = bases[0]**2
-        elif x*-1 > abs(y) or x >= abs(y):
-            r = (abs(bases[0])-2)**2+1 + abs(bases[0])
-            if x < 0: # left quadrant
-                r = r - y + x - 1
-            else: # right quadrant
-                r = r + y - x - 3
+        elif x*-1 > abs(y) or x >= abs(y): # left and right
+            sign = cmp(bases[0], 0)
+            r = (bases[0]-2*sign)**2 + abs(bases[0]) + y*sign - x*sign - sign-1
         elif y*-1 >= abs(x) or y > abs(x): # top and bottom
-            lean = bases[1]/abs(bases[1])
-            r = bases[1]**2 + abs(bases[1]) - x*lean - y*lean * 3
+            sign = cmp(bases[1] , 0)
+            r = bases[1]**2 + abs(bases[1]) - x*sign - y*sign * 3
         else:
             r = -1
             print >> sys.stderr, "Totally shouldn't happen %d,%d" % (x, y)
